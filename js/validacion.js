6b1ac9da2279ab7f3fea68779e3b5d17da7c10f9
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2025-10-01 12:36:23 +0200

    Update de comentarios
    Actualizados los comentarios que devuelve el programa enfocados a los astronautas

--- js/validacion.js
+++ js/validacion.js
@@ -16,7 +16,7 @@ const comentarios = document.getElementById('comentarios')
 nombre.addEventListener("input", (e) => {
     const patron = /^[A-Za-z]{3,}$/;
     if (!patron.test(e.target.value)) {
-        nombre.setCustomValidity("Se espera un nombre al menos 3 letras")
+        nombre.setCustomValidity("Se espera un nombre al menos 3 letras futuro explorador espacial")
     } else {
         nombre.setCustomValidity("");
     }
@@ -26,7 +26,7 @@ nombre.addEventListener("input", (e) => {
 email.addEventListener("input", (e) => {
     const patron = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (!patron.test(e.target.value)) {
-        email.setCustomValidity("Se espera un email valido")
+        email.setCustomValidity("Se espera un email valido para poder comunicarnos con un gran futuro(posible) astronauta")
     } else {
         email.setCustomValidity("");
     }
@@ -36,7 +36,7 @@ email.addEventListener("input", (e) => {
 contrasenia.addEventListener("input", (e) => {
     const patron = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!patron.test(e.target.value)) {
-        contrasenia.setCustomValidity("Tu contraseña debe contener almenos una mayuscula, una minuscula, un numero, un caracter especial valido y tener al menos 8 caracteres");
+        contrasenia.setCustomValidity("Tu contraseña debe contener almenos una mayuscula, una minuscula, un numero, un caracter especial valido y tener al menos 8 caracteres. No es tan dificl, tu puedes ;)");
     } else {
         contrasenia.setCustomValidity("");
     }
@@ -45,7 +45,7 @@ contrasenia.addEventListener("input", (e) => {
 
 edad.addEventListener("input", (e) => {
     if (e.target.value < 25 || e.target.value > 50) {
-        edad.setCustomValidity("La edad debe estar entre 25 y 50");
+        edad.setCustomValidity("La edad debe estar entre 25 y 50, al espacio solo puede ir gente entre esas edades..., es una norma no escrita");
     } else {
         edad.setCustomValidity("");
     }
@@ -55,7 +55,7 @@ edad.addEventListener("input", (e) => {
 fecha.addEventListener("input", (e) => {
     const hoy = new Date().toISOString().split("T")[0]; // formato YYYY-MM-DD
     if (e.target.value < hoy) {
-        fecha.setCustomValidity("La fecha no puede ser anterior a la actual");
+        fecha.setCustomValidity("La fecha no puede ser anterior a la actual, a no ser que seas un viajero del tiempo de sugiero que la cambies");
     } else {
         fecha.setCustomValidity("");
     }
@@ -63,7 +63,7 @@ fecha.addEventListener("input", (e) => {
 
 condicion.addEventListener("input", (e) => {
     if (e.target.value < 7) {
-        condicion.setCustomValidity("La condicion fisica debe ser mayor o igual a 7");
+        condicion.setCustomValidity("La condicion fisica debe ser mayor o igual a 7, aunque parezca mentira en el espacio debes estar en forma");
     } else {
         condicion.setCustomValidity("");
     }
@@ -79,14 +79,14 @@ expediente.addEventListener("input", (e) => {
         expediente.setCustomValidity("");
 
     } else {
-        expediente.setCustomValidity("Solo se permiten archivos pdf");
+        expediente.setCustomValidity(`Solo se permiten archivos pdf, se que te gusta mucho ${extension} pero los astronautas de verdad usan pdfs`);
     }
 
 })
 
 comentarios.addEventListener("input", (e) => {
     if (e.target.value.length < 10) {
-        comentarios.setCustomValidity("Los comentarios deben tener al menos 10 caracteres");
+        comentarios.setCustomValidity("Los comentarios deben tener al menos 10 caracteres, sí, nos gusta que te explayes al igual que el universo");
     } else {
         comentarios.setCustomValidity("");
     }
@@ -94,12 +94,4 @@ comentarios.addEventListener("input", (e) => {
 })
 
 
-const validarCondicion = (e) => {
-    let resultado = false
-    let condicion = document.getElementById("condicionFisica").value
-
-    if (condicion >= 7) resultado = true
-
-    return resultado
-}
 
